0bb6d7af7b5f65e61ca80c7f7ed557d956207887
jazwu <jaz.wu@outlook.com>
2024-07-27 20:52:59 +0800

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -7,13 +7,14 @@ import bodyParser from "body-parser";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log("Failed to connect to MongoDB", err);
-    });
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 
 const app = express();
 const PORT = 3000;
@@ -21,8 +22,15 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({ success: false, statusCode, message });
+});
