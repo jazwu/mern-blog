347f44106b8cbfac84687710adc530e6a1a14712
jazwu <jaz.wu@outlook.com>
2024-08-01 10:39:09 +0800

    create signup page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
       </Button>
       <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon/>
+          <FaMoon />
         </Button>
         <Link to="/sign-in">
           <Button gradientDuoTone="purpleToBlue" outline>
@@ -43,22 +43,16 @@ export default function Header() {
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-          <Navbar.Link active={path === "/"} as={"div"}>
-            <Link to="/">
-              Home
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/about"} as={"div"}>
-            <Link to="/about">
-              About
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/projects"} as={"div"}>
-            <Link to="/projects">
-              Projects
-            </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
