e0553a8b7ddec473e9d375c641a9e61f2c2ed48e
jazwu <jaz.wu@outlook.com>
2024-08-08 11:39:45 +0800

    complete dark mode functionality

---
+++ client/src/components/Theme.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+
+export default function ThemeProvider({ children }) {
+  const { theme } = useSelector((state) => state.theme);
+
+  return (
+    <div className={theme}>
+      <div className="bg-white dark:bg-[rgb(16,23,42)] text-gray-700 dark:text-gray-200 min-h-screen">
+        {children}
+      </div>
+    </div>
+  );
+}
