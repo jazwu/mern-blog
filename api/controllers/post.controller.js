58a6a004b1044e060a4eab579bb73f6eafab6f19
jazwu <jaz.wu@outlook.com>
2024-09-05 23:07:05 -0400

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,7 +2,6 @@ import { errorHandler } from "../utils/error.js";
 import Post from "../models/post.model.js";
 
 export const create = async (req, res, next) => {
-
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not authorized to create a post"));
   }
@@ -24,6 +23,37 @@ export const create = async (req, res, next) => {
   try {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getposts = async (req, res, next) => {
+  const limit = parseInt(req.query.limit) || 9;
+  const startIndex = parseInt(req.query.startIndex) || 0;
+  const sortDirection = req.query.sortDirection === "asc" ? 1 : -1;
+  try {
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    
+    const totalPosts = await Post.countDocuments();
+    const oneMonthAgo = new Date().setMonth(new Date().getMonth() - 1);
+    const lastMonthPosts = await Post.countDocuments({ createdAt: { $gte: oneMonthAgo } });
+
+    res.status(200).json({ posts, totalPosts, lastMonthPosts });
   } catch (error) {
     next(error);
   }
