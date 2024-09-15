2cda58803f00c19a3dd9cf8ca75fec30b38a370a
jazwu <jaz.wu@outlook.com>
2024-09-15 15:17:47 -0400

    add recent article section to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,14 +3,16 @@ import { useState, useEffect } from "react";
 import { Spinner, Button } from "flowbite-react";
 import { Link } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
-import DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -33,6 +35,21 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    const fetchRecentPosts = async () => {
+      try {
+        const response = await fetch("/api/post/getposts?limit=3");
+        if (response.ok) {
+          const data = await response.json();
+          setRecentPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchRecentPosts();
+  }, [recentPosts]);
 
   return (
     <div className="container mx-auto">
@@ -61,19 +78,33 @@ export default function PostPage() {
             className="mt-10 p-3 max-h-[600px] w-full object-cover"
           />
           <div className="flex justify-between p-3 border-b mx-auto w-full max-w-2xl">
-            <span className="font-serif">{new Date(post.createdAt).toLocaleDateString()}</span>
+            <span className="font-serif">
+              {new Date(post.createdAt).toLocaleDateString()}
+            </span>
             <span className="font-serif italic">
               {(post.content.length / 1000).toFixed(0)} mins read
             </span>
           </div>
           <div
             className="p-3 max-w-2xl w-full mx-auto post-content"
-            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content) }}
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(post.content),
+            }}
           ></div>
           <div className="max-w-4xl mx-auto">
             <CallToAction />
           </div>
           <CommentSection postId={post._id} />
+          <div className="flex flex-col justify-center items-center mb-5">
+            <h1 className="text-xl mt-5">Recent articles</h1>
+            <div className="flex flex-wrap gap-5 mt-5 justify-center">
+              {recentPosts &&
+                recentPosts.map((post) => (
+                  <PostCard key={post._id} post={post} />
+                ))
+              }
+            </div>
+          </div>
         </main>
       )}
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,5 +13,6 @@ export default {
   plugins: [
     flowbite.plugin(),
     require("tailwindcss-scrollbar"),
+    require('@tailwindcss/line-clamp'),
   ],
 };
